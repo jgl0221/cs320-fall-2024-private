6d435539e5486aed4b227643ef4cfbb54e478f45
Joshua Lee <jlee022103@gmail.com>
2024-09-20 23:50:45 -0400

    completed problem 2 yay

--- assigns/assign02/lib/assign02_02.ml
+++ assigns/assign02/lib/assign02_02.ml
@@ -1,8 +1,24 @@
 type matrix = {
-  entries : float list list;
-  rows : int;
-  cols : int;
+entries : float list list;
+rows : int;
+cols : int;
 }
 
+(* recursive function goes through entries, returns empty list if no entries*)
+let rec sublists list n = 
+  match list with
+  | [] -> []
+  | _ ->
+(* else create rows with the first n entries, and make rest list with n entries. concatenate to row. List.take creates
+a new list, so every n entries taken is a new row *)
+    let row = List.take n list in
+    let rest = List.drop n list in
+    row :: sublists rest n
+
 let mk_matrix entries (r, c) =
-  
+(* set n to be c which represents number of columns. number of columns represents number of values within each tuple/row *)
+  {
+    entries = sublists entries c;
+    rows = r;
+    cols = c;
+  }
